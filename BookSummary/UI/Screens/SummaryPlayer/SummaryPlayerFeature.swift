8a0c16c23872bd5b0a007a367cecf6dc3f64d98e
roman-leibiuk <roman.leibiuk@gmail.com>
2025-02-15 15:45:37 +0100

    Step 1: Setting up the architecture (#1)
    * feat: add git ignore file
    
    * feat: introduced TCA SPM
    
    * feat: introduced base object and file structure
    
    * feat: introduce colors and app spacer

---
+++ BookSummary/UI/Screens/SummaryPlayer/SummaryPlayerFeature.swift
@@ -0,0 +1,32 @@
+//
+//  SummaryPlayerFeature.swift
+//  BookSummary
+//
+//  Created by Roman Leibiuk on 15.02.2025.
+//
+
+import Foundation
+import ComposableArchitecture
+import SwiftUI
+
+@Reducer
+struct SummaryPlayerFeature {
+    
+    @ObservableState
+    struct State {
+        
+    }
+    
+    enum Action {
+        
+    }
+    
+    var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+            default:
+                break
+            }
+        }
+    }
+}
