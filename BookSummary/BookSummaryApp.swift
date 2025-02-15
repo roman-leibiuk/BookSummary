f26e177194c75395c0cc24d3edb28c2c63b1a2af
roman-leibiuk <roman.leibiuk@gmail.com>
2025-02-15 12:51:40 +0100

    Initial Commit

---
+++ BookSummary/BookSummaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookSummaryApp.swift
+//  BookSummary
+//
+//  Created by Roman Leibiuk on 15.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct BookSummaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
