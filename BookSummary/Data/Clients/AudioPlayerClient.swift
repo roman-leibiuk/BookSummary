52e7723f14e3ca732704288d757dcf0650639a26
roman-leibiuk <roman.leibiuk@gmail.com>
2025-02-17 15:55:17 +0100

    introduced AudioPlayerClient

--- BookSummary/Data/Clients/AudioPlayerClient.swift
+++ BookSummary/Data/Clients/AudioPlayerClient.swift
@@ -5,4 +5,43 @@
 //  Created by Roman Leibiuk on 15.02.2025.
 //
 
-import Foundation
+import AVFoundation
+import Dependencies
+
+extension DependencyValues {
+    var audioPlayerClient: AudioPlayerClient {
+        get { self[AudioPlayerClient.self] }
+        set { self[AudioPlayerClient.self] = newValue }
+    }
+}
+
+public struct AudioPlayerClient {
+    public var play: @Sendable (ChapterModel) async -> AsyncStream<PlayerAction> = { _ in .finished }
+    public var resume: @Sendable () async -> Void
+    public var pause: @Sendable () async -> Void
+    public var fastForward: @Sendable (Double) async -> Void
+    public var rewind: @Sendable (Double) async -> Void
+    public var seekTo: @Sendable (TimeInterval) async -> Void
+    public var elapsedTime: @Sendable () async -> TimeInterval = { 0 }
+    public var totalTime: @Sendable () async -> TimeInterval = { 0 }
+    public var elapsedTimeUpdates: @Sendable () async -> AsyncStream<TimeInterval> = { .finished }
+    public var playbackRate: @Sendable (Float) async -> Void
+}
+
+extension AudioPlayerClient: DependencyKey {
+    public static var liveValue: AudioPlayerClient {
+        let audioActor = AudioActor()
+        return Self(
+            play: { await audioActor.play(chapter: $0) },
+            resume: { await audioActor.resume() },
+            pause: { await audioActor.pause() },
+            fastForward: { await audioActor.fastForward(seconds: $0) },
+            rewind: { await audioActor.rewind(seconds: $0) },
+            seekTo: { await audioActor.seekTo($0) },
+            elapsedTime: { await audioActor.elapsedTime()},
+            totalTime: { await audioActor.totalTime() },
+            elapsedTimeUpdates: { await audioActor.elapsedTimeUpdates() },
+            playbackRate: { await audioActor.setPlaybackRate($0) }
+        )
+    }
+}
